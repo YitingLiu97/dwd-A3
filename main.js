94622f97551d2fc8221a7221ed053373a955abe6
YitingLiu97 <54900403+YitingLiu97@users.noreply.github.com>
2020-02-23 11:31:29 -0500

    groups of images shown, but another clicks of button won't work.

--- main.js
+++ main.js
@@ -1,67 +1,51 @@
 //get photos from Flickr
 /***click the button and REMOVE an element on the page **/
 window.addEventListener('DOMContentLoaded', () => {
-    const button = document.getElementById("fun-button");
-    /***remove div - method 2 ***/
-    button.onclick = buttonHandler;
+  const button = document.getElementById("submit");
+  button.onclick = buttonHandler;
+
 });
 
-
-
-// let APIKey=""
-// let url=https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=1f761e19e3a3928e3438c53dd17b82c5&tags=cat&safe_search=1&per_page=10&page=2&format=json
-// https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}.jpg
-
-let APIKey="1f761e19e3a3928e3438c53dd17b82c5";
-let tags =document.getElementById('textInput').textContent;
+let tags;
+let APIKey = "1f761e19e3a3928e3438c53dd17b82c5";
+// let tags ="cat";
 let perPageLimits = 10;
+let farmid = [];
+let serverid = [];
+let id = [];
+let secret = [];
 
-let farmid=[];
-let serverid=[];
-let id=[];
-let secret=[];
-
-let url=`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${APIKey}&tags=${tags}&safe_search=1&per_page=${perPageLimits}&page=2&format=json&nojsoncallback=1`;
 
 
 async function buttonHandler() {
-    const response = await fetch(url);
-    const results= await response.json();
-    // console.log(photos.photos.photo[0].id);
+  tags = document.getElementById("textInput").value;
+  console.log(tags);
 
-    let photos = results.photos.photo;
-    // let photosLength = photos.length;
+  let url = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${APIKey}&tags=${tags}&safe_search=1&per_page=${perPageLimits}&page=2&format=json&nojsoncallback=1`;
 
-    let photoURL =[];
+  const response = await fetch(url);
+  const results = await response.json();
+  // console.log(photos.photos.photo[0].id);
 
+  let photos = results.photos.photo;
+  // let photosLength = photos.length;
 
-    for (let i =0; i< photos.length;i++){
-        farmid=photos[i].farm;
-        serverid=photos[i].server;
-        secret=photos[i].secret;
-        id=photos[i].id;
-        photoURL.push(`https://farm${farmid}.staticflickr.com/${serverid}/${id}_${secret}.jpg`);
-        
-      
-        let imageSrc=`${photoURL[2]}`;
-        
-        document.getElementById('images').src=imageSrc;
+  let photoURL = [];
 
-
-        
-        // return imageSrc;
-    
-    
+  for (let i = 0; i < photos.length; i++) {
+    farmid = photos[i].farm;
+    serverid = photos[i].server;
+    secret = photos[i].secret;
+    id = photos[i].id;
+    photoURL.push(`https://farm${farmid}.staticflickr.com/${serverid}/${id}_${secret}.jpg`);
    
-   
+    for (let l=0; l<photoURL.length;l++){
+      let onepic = document.createElement("img");
+      document.getElementById("pictures").appendChild(onepic);
+      onepic.src=`${photoURL[l]}`;
+    }
     }
 
-   
-    // return `<p>${photos.photos.photo[2].title}<p>`;
-  }
-
-  function show(){
-
-    // console.log(photoURL);
-
-  }
+  
+    
+    }   
